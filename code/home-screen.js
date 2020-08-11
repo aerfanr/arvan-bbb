502420e0abd46675ddbb08d472d00158c793c619
Amirerfan Rafati <erfan.rafati@outlook.com>
2020-08-11 19:16:40 +0430

    Implemented BBB status fetch.

--- code/home-screen.js
+++ code/home-screen.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { View, Text, Alert } from 'react-native'
 import * as Keychain from 'react-native-keychain'
 import axios from 'axios'
+import sha1 from 'js-sha1'
+import { parseString } from 'react-native-xml2js'
 
 import styles from './styles'
 
@@ -27,7 +29,35 @@ const HomeScreen = () => {
                 throwError('Arvan Error', 'No Arvan Credentials')
             }
         } catch (error) {
-            throwError('Arvan Error', error)
+            throwError('Arvan Error', JSON.stringify(error))
+        }
+    }
+
+    const getBbbStatus = async () => {
+        try {
+            const credentials = await Keychain.getGenericPassword({ service: 'bbb' })
+            if (credentials) {
+                const query = credentials.username + 'api/getMeetings?checksum=' + sha1('getMeetings' + credentials.password)
+                axios.get(query)
+                    .then(res => {
+                        parseString(res.request._response, (error, result) => {
+                            console.log(error)
+                            if (result.response.meetings[0]) {
+                                setMeetings(result.response.meetings.length)
+                                let count = 0
+                                for (const meeting of result.response.meetings) {
+                                    count += Number(meeting.meeting[0].participantCount)
+                                }
+                                setParticipants(count)
+                            }
+                        })
+                    })
+                    .catch(error => throwError('BigBlueButton Error', error.message))
+            } else {
+                throwError('BigBlueButton Error', 'No BigBlueButton credentials')
+            }
+        } catch (error) {
+            throwError('BigBlueButton error', JSON.stringify(error))
         }
     }
 
@@ -37,13 +67,18 @@ const HomeScreen = () => {
 
     useEffect(() => {
         getServerStatus()
+        getBbbStatus()
     }, [])
 
     const [serverStatus, setServerStatus] = useState('')
+    const [meetings, setMeetings] = useState(0)
+    const [participants, setParticipants] = useState(0)
 
     return (
         <View style={styles.container}>
             <Text> {serverStatus} </Text>
+            <Text> {meetings ? meetings + ' meeting(s) are running.' : 'There is not any meetings.'} </Text>
+            <Text> {participants ? participants + ' participant(s) are online.' : 'There is no participant online.'} </Text>
         </View>
     )
 }
