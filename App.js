5998bac284f60b4bb6d74163045bbe9edb78b134
Amirerfan Rafati <erfan.rafati@outlook.com>
2020-08-13 15:58:15 +0430

    Added icons and splash screen files

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-
 import SettingsScreen from './code/settings-screen'
 import HomeScreen from './code/home-screen'
 
